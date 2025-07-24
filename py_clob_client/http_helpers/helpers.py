000fd31fe641cafb8a79904f20802e82cca0b524
felixjchen <31393977+felixjchen@users.noreply.github.com>
2025-07-24 13:43:48 -0400

    Update helpers.py

--- py_clob_client/http_helpers/helpers.py
+++ py_clob_client/http_helpers/helpers.py
@@ -36,17 +36,16 @@ def request(endpoint: str, method: str, headers=None, data=None):
     try:
         headers = overloadHeaders(method, headers)
         print(method, endpoint, headers, data)
-        # resp = requests.request(
-        #     method=method, url=endpoint, headers=headers, json=data if data else None
-        # )
-        # if resp.status_code != 200:
-        #     raise PolyApiException(resp)
+        resp = requests.request(
+            method=method, url=endpoint, headers=headers, json=data if data else None
+        )
+        if resp.status_code != 200:
+            raise PolyApiException(resp)
 
-        # try:
-        #     return resp.json()
-        # except requests.JSONDecodeError:
-        #     return resp.text
-        return PolyApiException("test")
+        try:
+            return resp.json()
+        except requests.JSONDecodeError:
+            return resp.text
 
     except requests.RequestException:
         raise PolyApiException(error_msg="Request exception!")
